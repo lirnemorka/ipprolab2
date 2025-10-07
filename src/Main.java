a9023a5d3b87525cc23e6564210999e6778e1f62
lirnemor <kuratnik010206@gmail.com>
2025-10-07 17:35:09 +0300

    stash

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is the main branch!");
+        System.out.println("Some temporary changes!");
     }
 }
